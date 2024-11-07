f92e11f831f1aabf78302b5841cc6c03ab16667e
Giovanni <158181635+giovannig10@users.noreply.github.com>
2024-11-07 11:44:36 -0300

    feat: create post validation

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -6,31 +6,25 @@ let usuarios = [];
 
 usuariosRoutes.get("/", (req, res) => {
   return res.status(200).json({
-    message: usuarios.length == 0 
-    ? "Nenhum usuário cadastrado" 
-    : `Total de usuários cadastrados: ${usuarios.length}`, 
-    usuarios, 
-    });
-});
-
-usuariosRoutes.post("/", (req, res) => {
-  const {destino, orcamento, status, objetivosViagem } = req.body;
-
-  const novousuario = {
-    id: Math.floor(Math.random() * 1000000),
-    destino,
-     orcamento,
-     status, 
-     objetivosViagem 
-  };
-
-  // Adiciona o novo usuario ao array de usuarios
-  usuarios.push(novousuario);
-
-  return res.status(201).json({
-    message: "usuario cadastrado com sucesso!",
-    novousuario,
+    message: usuarios.length == 0
+      ? "Nenhum usuário cadastrado"
+      : `Total de usuários cadastrados: ${usuarios.length}`,
+    usuarios,
   });
 });
-
+usuariosRoutes.post("/", (req, res) => {
+  const { destino, orcamento, status, objetivoViagem } = req.body;
+  if (!destino || !orcamento) {
+    return res.status(400).json({ message: "Preencha os campos obrigatorios corretamente" });
+  }
+  if (status != "iniciada" && status != "planejando" && status != "finalizada") {
+    return res.status(400).json({ message: "Status inválido" });
+    }
+    if (!objetivoViagem || objetivoViagem.length < 2) {
+    return res.status(400).json({ message: "O campo objetivoViagem deve conter no mínimo dois objetivos" });
+  }
+  const novoUsuario = { id: usuarios.length + 1, destino, orcamento, status, objetivoViagem };
+  usuarios.push(novoUsuario);
+  return res.status(201).json({ message: "Usuário cadastrado com sucesso", usuario: novoUsuario });
+});
 export default usuariosRoutes;
